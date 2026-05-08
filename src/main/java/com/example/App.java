43e1793a26a2023f7db86bb5518623e852cfc83b
Yonan Smith Cuba LLacsahuache <yonansmithcuballacsahuache@gmail.com>
2026-05-07 22:11:19 -0500

    Init commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hola CI/CD");
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testSumar() {
+
+        App app = new App();
+
+        int resultado = app.sumar(2, 3);
+
+        assertEquals(5, resultado);
+    }
+}
